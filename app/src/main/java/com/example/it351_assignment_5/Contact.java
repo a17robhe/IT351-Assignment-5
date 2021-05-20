115b4d9b9db55c765782c9e75eba9a9cd79436a2
robin <a17robhe@student.his.se>
2021-05-20 18:01:59 +0200

    List fetching json data implemented

---
+++ app/src/main/java/com/example/it351_assignment_5/Contact.java
@@ -0,0 +1,36 @@
+package com.example.it351_assignment_5;
+
+public class Contact {
+    private String name;
+    private String address;
+    private String company;
+    private String phoneNumber;
+
+    public Contact(String name, String address, String company, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.company = company;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCompany(){
+        return company;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " +  address + " - " + company + " - " + phoneNumber;
+    }
+}
--- app/src/main/java/com/example/it351_assignment_5/MainActivity.java
+++ app/src/main/java/com/example/it351_assignment_5/MainActivity.java
@@ -2,13 +2,102 @@ package com.example.it351_assignment_5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<Contact> contacts;
+    ArrayAdapter<Contact> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contacts = new ArrayList<Contact>();
+        adapter = new ArrayAdapter<Contact>(this, android.R.layout.simple_list_item_1, contacts);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+        new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=a17robhe");
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            try {
+                JSONArray arr = (JSONArray) new JSONTokener(json).nextValue();
+                for (int i = 0; i < arr.length(); i++){
+                    JSONObject element = arr.optJSONObject(i);
+                    String name = element.getString("name");
+                    String company = element.getString("company");
+                    String address = element.getString("location");
+                    String phoneNumber = element.getString("auxdata");
+
+                    contacts.add(new Contact(name,address,company, phoneNumber));
+                }
+                adapter.notifyDataSetChanged();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
+
+
